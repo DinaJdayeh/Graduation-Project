e8ebb4eb84761fa357c4b1bea4172cff2695bba6
DinaJdayeh <dinajdayeh@gmail.com>
2019-10-07 23:19:51 +0300

    Graduation Project

---
+++ loca/loca/loca/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Plugin.Geolocator;
+
+namespace loca
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnGetLocation.Clicked += BtnGetLocation_Clicked;
+
+        }
+
+
+        private async void BtnGetLocation_Clicked(object sender, EventArgs e)
+        {
+            await RetreiveLocation();
+        }
+
+        private async Task RetreiveLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(100));
+
+            txtLat.Text = "Latitude: " + position.Latitude.ToString();
+            txtLong.Text = "Longitude: " + position.Longitude.ToString();
+
+           MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude)
+                             , Distance.FromMiles(1)));
+        }
+
+
+
+    }
+}
